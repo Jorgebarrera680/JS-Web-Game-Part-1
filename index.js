3a9cbfbd3e7eb8eda5290676ba0a0336fc0d3045
jorge <jorgebarrera680@gmail.com>
2022-08-06 21:33:41 -0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, left, bottom){
+    let smImg = document.createElement('img')
+    smImg.src = src
+    smImg.style.position = 'fixed'
+    smImg.style.left = left + 'px'
+    smImg.style.bottom = bottom + 'px'
+    document.body.append(smImg)
+    return smImg
+}
+
+newImage('assets/green-character.gif', 100,100)
+newImage('assets/tree.png',200,300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png',450,200)
+newImage('assets/crate.png',150,200)
+newImage('assets/well.png',500,425)
+newImage('assets/sword.png', 500, 405)
+newImage('assets/shield.png', 165, 185)
+newImage('assets/staff.png', 600, 100)
+
+    
+
+
+
+
+
